8f547679fdb1c3ceead161b9db3bcaedba91db47
HigorG26 <you@example.com>
2023-08-21 22:16:06 -0300

    Desafio

---
+++ Aula2/index.js
@@ -0,0 +1,6 @@
+var n1 = 2;
+var n2 = 3;
+function greeting(n1, n2) {
+    console.log("Resultado: ", n1 + n2);
+}
+greeting(n1, n2);
---
+++ Aula2/index.ts
@@ -0,0 +1,8 @@
+const n1 = 2
+const n2 = 3
+
+function greeting(n1: number, n2:number) {
+    console.log("Resultado: ", n1+n2)
+}
+
+greeting(n1, n2)
