3ef42287ae2beed79bf1f32da644ea1879d90f9f
Higor Gabriel <e_higorgfs@sebraesp.com.br>
2023-08-22 10:18:58 -0300

    Fundamentos

---
+++ Aula3_Fundamentos/index.js
@@ -0,0 +1,22 @@
+//Numeros - Numbers
+var x = 10;
+console.log("X = ", x);
+x = 15;
+console.log(typeof x);
+var y = 14.52537;
+console.log(y.toPrecision);
+console.log("Y = ", y);
+console.log(typeof y);
+//---------------------------------
+//STRING
+var nome1 = "Higor";
+var nome2 = "Gabriel";
+var fullName = nome1 + " " + nome2;
+console.log(fullName);
+//---------------------------------
+//BOOLEAN
+var a = false;
+console.log("1º: ", a);
+console.log(typeof a);
+a = true;
+console.log("2º: ", a);
---
+++ Aula3_Fundamentos/index.ts
@@ -0,0 +1,33 @@
+//Numeros - Numbers
+let x: number = 10;
+console.log("X = ", x)
+
+x = 15
+console.log(typeof x)
+
+const y:number=14.52537
+
+console.log(y.toPrecision)
+console.log("Y = ", y)
+console.log(typeof y)
+
+//---------------------------------
+//STRING
+
+const nome1: string = "Higor"
+const nome2: string = "Gabriel"
+
+let fullName:string = nome1 + " " + nome2
+console.log(fullName)
+
+//---------------------------------
+//BOOLEAN
+
+let a: boolean = false
+
+console.log("1º: ",a)
+console.log(typeof a)
+
+a = true
+
+console.log("2º: ",a)
