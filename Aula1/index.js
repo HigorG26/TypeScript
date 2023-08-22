5c0cb8fa0595e1d0b2e8efd81b1179ad7b378c58
HigorG26 <you@example.com>
2023-08-21 22:01:12 -0300

    Aula 1 - O que é o TS

---
+++ Aula1/index.js
@@ -0,0 +1,8 @@
+var firstName = "Higuinho";
+var another = 20;
+var x = true;
+function greeting(firstName) {
+    console.log("Bem vindo ao Vasco: ", firstName);
+}
+greeting(firstName);
+// greeting(another)
---
+++ Aula1/index.ts
@@ -0,0 +1,10 @@
+const firstName = "Higuinho";
+const another = 20;
+const x = true;
+
+function greeting(firstName: string) {
+    console.log("Bem vindo ao Vasco: ", firstName)
+}
+
+greeting(firstName)
+// greeting(another)
