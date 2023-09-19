b168545e89fe6b1b33a25c88e27f127ba8940262
Higor Gabriel <e_higorgfs@sebraesp.com.br>
2023-09-19 12:20:18 -0300

    W2H

--- Aula9_Class/index.js
+++ Aula9_Class/index.js
@@ -1,3 +1,29 @@
 "use strict";
-class Userr {
+class blogSpot {
+    constructor(title) {
+        this.title = title;
+    }
+    itemTitle() {
+        return `O titulo do pst é: ${this.title}`;
+    }
+}
+// public method
+class A {
+    constructor() {
+        this.x = 10;
+    }
+}
+class B {
+    constructor() {
+        this.x = 15;
+    }
+}
+const Aa = new A();
+const Bb = new B();
+console.log(Aa);
+// statis member
+class StaticsMembers {
+    constructor() {
+        this.prop = "teste estatitco";
+    }
 }
--- Aula9_Class/index.ts
+++ Aula9_Class/index.ts
@@ -1,4 +1,32 @@
-class Userr {
-    name!: string
-    age!: number
+interface showTitle{
+    itemTitle() : string
+}
+class blogSpot implements showTitle {
+    title
+
+    constructor(title:showTitle) {
+        this.title = title
+    }
+    itemTitle() {
+        return `O titulo do pst é: ${this.title}`
+    }
+}
+
+// public method
+ class A {
+    public x = 10
+ }
+
+ class B {
+    public x = 15
+ }
+
+ const Aa = new A()
+ const Bb = new B()
+ console.log(Aa)
+
+// statis member
+
+class StaticsMembers {
+    prop = "teste estatitco"
 }
