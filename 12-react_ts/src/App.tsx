f3879e7cb0ee8684db8815e4e72c78a38e9397d4
HigorG26 <higor.gabrielfds@gmail.com>
2023-10-29 18:53:35 -0300

    H2W

--- 12-react_ts/src/App.tsx
+++ 12-react_ts/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className=''>Typescript com React</h1>
     </div>
   );
 }
