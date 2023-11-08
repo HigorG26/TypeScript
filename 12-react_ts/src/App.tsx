dd16c32c01392741bd41e453fb08825c15314847
Higor Gabriel <e_higorgfs@sebraesp.com.br>
2023-11-08 15:48:26 -0300

    W2H

--- 12-react_ts/src/App.tsx
+++ 12-react_ts/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useState} from 'react';
+import FisrtComponent from './components/fisrtComponent';
 
 function App() {
+  //variaveis
+  const name: string = 'Higor'
+
+
+
+  const userGreeting = (name:string):string => {
+    return `Olá, ${name}`
+  }
+
+
+
   return (
     <div className="App">
       <h1 className=''>Typescript com React</h1>
+      <h2>Nome: {name}</h2>
+      <h2>{userGreeting(name)}</h2>
+      <FisrtComponent/>
     </div>
   );
 }
---
+++ 12-react_ts/src/components/fisrtComponent.tsx
@@ -0,0 +1,11 @@
+//componente
+import React, { ReactElement } from 'react'
+
+
+function FisrtComponent(): ReactElement {
+  return (
+    <div>Meu primeiro componente.</div>
+  )
+}
+
+export default FisrtComponent
