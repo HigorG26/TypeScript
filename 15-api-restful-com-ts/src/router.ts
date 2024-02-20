974f31ba44f4bed2db32f48965897f9a57037116
HigorG26 <higor.gabrielfds@gmail.com>
2024-02-20 01:46:24 -0300

    H2H

--- 15-api-restful-com-ts/src/router.ts
+++ 15-api-restful-com-ts/src/router.ts
@@ -1,12 +1,17 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/movieControllers";
+
+//Validations
+import { validate } from "./middleware/handleValidator";
+import { movieCreateValidation } from "./middleware/movieValidations";
 
 
 const router = Router();
 
-export default router.get("/", (req: Request, res: Response) => {
+export default router.get("/test", (req: Request, res: Response) => {
     if (res.status(200)) {
         res.send("API Online!")
     }else {
         res.send("API Offline")
     }
-})
+}).post("/movie", movieCreateValidation(), validate, createMovie)
